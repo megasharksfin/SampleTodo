f2d5292fdd7c520c01f74694747ce3a9b5387da1
Manalo <r.manalo@fujitsu.com>
2020-01-31 09:06:24 +0800

    Commit files

---
+++ frontend/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/styles/styles.css">
+  <script src="./assets/js/script.js"></script>
+  <script src="./assets/js/jquery-3.4.1.min.js"></script>
+  <title> Sample Todo </title>
+</head>
+<body>
+  <div class="scroll">
+    <div class="wrapper">
+      <h2> Sample Todo Application </h2>
+      <div class="horizontal-rule"></div>
+      <form action="" class="login-form">
+        <div class="form-wrapper">
+          <label for="username"> Username </label>
+          <input type="text" name="username" id="username" class="textfield" placeholder="Input username here">
+        </div>
+        <div class="form-wrapper">
+          <label for="password"> Password </label>
+          <input type="password" name="pass" id="pass" class="textfield" placeholder="Input password here">
+        </div>
+        <div class="button-wrapper">
+          <button type="submit" class="submit-button"> Login </button>
+          <a href="#"> Create User </a>
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
